7b23e669da50717fb3d983befb153ca24abf44f8
Artista7 <shubham1.6180@gmail.com>
2019-02-21 00:54:10 +0530

    updated cognitoLambda:- 1. Added pre signup trigger to validate collegePasscode 2. Updated PostSignUp trigger to add college name and group to user

---
+++ packages/awsls/amplify/backend/function/cognitoLambda/src/preSignUp.js
@@ -0,0 +1,44 @@
+const aws = require('aws-sdk');
+const ddb = new aws.DynamoDB({ apiVersion: '2012-10-08' });
+const graphqlApiId = process.env.GraphQLApiId;
+const env = process.env.ENV;
+
+module.exports = function preSignUp(event, context, callback) {
+    //get college from from collegePasscode
+    const CollegeTableName = "College-" + graphqlApiId + "-" + env;
+    const collegePasscode = event.request.userAttributes['custom:collegePasscode'];
+
+    var params = {
+        ExpressionAttributeValues: {
+            ':sp': { S: collegePasscode }
+        },
+        ExpressionAttributeNames: { "#n": "name" },
+        ProjectionExpression: '#n, collegePasscode',
+        FilterExpression: 'contains (collegePasscode, :sp)',
+        TableName: CollegeTableName
+    };
+
+    console.log("3. College table scan params - ", params);
+
+    ddb.scan(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+            callback(err, event);
+        } else {
+            //if college present addcollege id to new user object
+            if (data.Items.length > 0) {
+                console.log("4. Success college name", data.Items[0]);
+                callback(null, event);
+            }
+            //else do not allow user signUp
+            else {
+                var error = new Error("College passcode cannot be verified");
+                // Return error to Amazon Cognito
+                callback(error, event);
+            }
+        }
+    });
+
+
+
+}
