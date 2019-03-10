514364d86c7eb46f6218a201d57a0af32d683485
Artista7 <shubham1.6180@gmail.com>
2019-03-11 03:33:51 +0530

    simple home page

--- packages/students/src/components/MainPage/HomePage/HomePage.jsx
+++ packages/students/src/components/MainPage/HomePage/HomePage.jsx
@@ -8,27 +8,19 @@ import wallpaper from '../../../assets/img/homepage.jpg';
 const HomePage = ({ userInfo, userPricingPlan }) => {
     return (
         <React.Fragment>
-            <div className="row" style={{ marginLeft: "-20px", marginRight: "-20px" }}>
-                <div className="col-sm-4 fullHeight" style={{ padding: 0 }}>
-                    <img src={wallpaper} className="fullHeight" style={{ position: "absolute", zIndex: "-1" }} alt="wallpaper is missing"></img>
-                    {userInfo.username && <div style={{ color: "#CACFD2" }}>
-                        {/* <p className="independent" style={{ bottom: "5%", fontSize: "28px" }}>{userInfo.username}</p> */}
-                        {/* <p className="independent">email - {userInfo.email}</p>
-                        <p className="independent">Phone No. - {userInfo.phone_number}</p> */}
-                        {userPricingPlan && <div className="independent" style={{ top: "40%", fontSize: "14px" }}>
-                            <svg height="80" width="80">
-                                <circle cx="40" cy="40" r="30" stroke="white" strokeWidth="3" fill="none" />
-                            </svg>
-                            <p style={{ marginTop: "-49px" }}>{userInfo.cvReviewsTaken} / {userPricingPlan.cvReviewsAllowed}</p>
-                            <p style={{ marginTop: "25px" }}>Cv Reviews Taken</p>
-                        </div>}
+            <div className="fullHeight" style={{ padding: 0, backgroundColor: "white", marginLeft: "-20px", marginRight: "-20px" }}>
+                <p className="independent" style={{ top: "30%", fontSize: "28px" }}>{userInfo.username}</p>
+                <p className="independent" style={{ top: "36%" }}>{userInfo.email}</p>
+                <p className="independent" style={{ top: "40%" }}>{userInfo.phone_number}</p>
+                {userInfo.username && <div>
+                    {userPricingPlan && <div className="independent" style={{ top: "45%", fontSize: "14px" }}>
+                        <svg height="80" width="80">
+                            <circle cx="40" cy="40" r="30" stroke="black" strokeWidth="3" fill="none" />
+                        </svg>
+                        <p style={{ marginTop: "-49px" }}>{userInfo.cvReviewsTaken} / {userPricingPlan.cvReviewsAllowed}</p>
+                        <p style={{ marginTop: "25px" }}>Cv Reviews Taken</p>
                     </div>}
-                </div>
-                <div className="col-sm-8 fullHeight" style={{ padding: 0, backgroundColor: "white" }}>
-                    <p className="independent" style={{ top: "40%", fontSize: "28px" }}>{userInfo.username}</p>
-                    <p className="independent" style={{ top: "45%" }}>{userInfo.email}</p>
-                    <p className="independent" style={{ top: "48%" }}>{userInfo.phone_number}</p>
-                </div>
+                </div>}
             </div>
         </React.Fragment>
     );
